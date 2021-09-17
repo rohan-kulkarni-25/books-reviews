830e3b1240097a17466dd593f1f4091139cfe446
Rohan Kulkarni <rohank2502@gmail.com>
2021-09-17 12:33:31 +0530

    FEAT : ADDED FRONTEND

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// IMPORTING MODULES
+// IMPORT REQUIRED MODULES
+
 const http = require('http')
 const url = require('url');
 const fs = require('fs');
@@ -9,23 +10,93 @@ const { hostname } = require('os');
 const booksData = fs.readFileSync(`${__dirname}/data/books-reviews.json`, 'utf-8');
 const booksObj = JSON.parse(booksData);
 
+// READING TEMPLATE FILES
+const tempBookshelf = fs.readFileSync(`${__dirname}/templates/template-bookshelf.html`, 'utf-8')
+const tempBookReview = fs.readFileSync(`${__dirname}/templates/template-book-review.html`, 'utf-8')
+const tempBookCard = fs.readFileSync(`${__dirname}/templates/template-book.html`, 'utf-8')
+const tempReviewCard = fs.readFileSync(`${__dirname}/templates/template-review.html`, 'utf-8')
+const tempBookInReviewCard = fs.readFileSync(`${__dirname}/templates/template-book-in-review.html`, 'utf-8')
+
+
+// Replace Module 
+// REPLACING PAGE 1 CONTENT 
+const templateCreator = (temp, data) => {
+  let bookCards = '';
+  data.forEach(element => {
+    let elementCard = temp.replace(/{%BOOKNAME%}/g, element.name);
+    elementCard = elementCard.replace(/{%BOOKNAME%}/g, element.name);
+    elementCard = elementCard.replace(/{%AUTHOR%}/g, element.author)
+    elementCard = elementCard.replace(/{%PRICE%}/g, element.price)
+    elementCard = elementCard.replace(/{%ID%}/g, element.id)
+    bookCards = bookCards.concat(elementCard);
+  });
+  return bookCards;
+}
+
+// REPLACING REVIEW PAGE CONTENT 
+const templateReviewCreator = (temp, data) => {
+  let elementCard = temp.replace(/{%BOOKNAME%}/g, data.name);
+  elementCard = elementCard.replace(/{%BOOKNAME%}/g, data.name);
+  elementCard = elementCard.replace(/{%AUTHOR%}/g, data.author)
+  elementCard = elementCard.replace(/{%PRICE%}/g, data.price)
+  elementCard = elementCard.replace(/{%ID%}/g, data.id)
+  return elementCard;
+}
+
+// REPLACING REVIEW CARDS 
+const templateReviewGenerator = (temp, data) => {
+  let reviewCards = '';
+  const dataObj = (data.reviews[0]).reader;
+  dataObj.forEach(reader => {
+    let Card = temp.replace(/{%REVIEW_NAME%}/g, reader.name);
+    Card = Card.replace(/{%REVIEW_COMMENT%}/g, reader.review);
+    reviewCards = reviewCards.concat(Card);
+  })
+  return reviewCards;
+}
 
 //  SERVER CREATED AND ROUTES ADDED 
 
 const server = http.createServer((req, res) => {
   const urlObj = url.parse(req.url, true);
-
   const pathName = urlObj.pathname;
 
-  if (pathName === '/') {
-    res.end('Please go to  /api');
+  // ROTUES START 
+
+  // BASIC ROUTE ENTRY POINT 
+  if (pathName === '/' || pathName === '/books') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    })
+    let output = templateCreator(tempBookCard, booksObj)
+    let outputPage = tempBookshelf.replace('{%BOOKS%}', output)
+    res.end(outputPage);
   }
+
+  // SPECIFIC BOOK ROUTE WHEN CLICKED ON READ REVIEW
+  else if (pathName === '/book/') {
+    let bookId = urlObj.query.id;
+    bookId--;
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    })
+    let output = templateReviewCreator(tempBookInReviewCard, booksObj[bookId]);
+    let outputPage = tempBookReview.replace('{%BOOK_CARD%}', output)
+    let reviews = templateReviewGenerator(tempReviewCard, booksObj[bookId]);
+    console.log(reviews);
+    outputPage = outputPage.replace(/{%ALL_REVIEWS%}/g, reviews);
+    res.end(outputPage);
+  }
+
+  // API ALL BOOKS LIST ROUTE 
   else if (pathName === '/api/books' || pathName === '/api') {
     res.writeHead(200, {
       'Content-type': 'Application/json'
     });
     res.end(booksData)
   }
+
+  // API SINGLE BOOK LIST ROUTE CAN GIVE ROUTE WITH HELP OF  PASSING ID IN QUERY 
   else if (pathName === '/api/book') {
     let bookId = urlObj.query.id;
     bookId--;
@@ -46,6 +117,8 @@ const server = http.createServer((req, res) => {
       res.end(bookData);
     }
   }
+
+  // ERROR CATCHING ROUTE 
   else {
     res.writeHead(404, {
       'Content-type': 'text/html'
